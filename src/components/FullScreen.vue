7719a96a6c8ff53f47db8c5a93b2986660ef4d15
lacroute <mc.flurry@hotmail.fr>
2019-05-22 14:27:53 +0200

    fix fullscreen heigth

--- src/components/FullScreen.vue
+++ src/components/FullScreen.vue
@@ -6,28 +6,27 @@
     @keyup.right="navigateFullscreen(1)"
     tabindex="0"
     class="fullscreen">
-    <div
-      class="wrapper-fullscreen">
-      <div class="controls">
-        <button
+      <div class="wrapper-header">
+        <header>
+          <button
           @click.stop.prevent="navigateFullscreen(-1)"
           type="button"
-          class="left"></button>
-        <button
+          class="controls left"></button>
+          <div class="text">
+            <h2>{{ fullScreenObject.title }}</h2>
+            <p>{{ fullScreenObject.photo.caption }}</p>
+          </div>
+          <button
           @click.stop.prevent="navigateFullscreen(1)"
           type="button"
-          class="right"></button>
+          class="controls right"></button>
+        </header>
       </div>
+    <div class="wrapper-view">
       <div class="view">
-        <div class="text">
-          <h2>{{ fullScreenObject.title }}</h2>
-          <p>{{ fullScreenObject.photo.caption }}</p>
-        </div>
-        <div class="pic">
-          <img
-            :src="fullScreenObject.photo.url"
-            alt="">
-        </div>
+        <img
+          :src="fullScreenObject.photo.url"
+          alt="">
       </div>
     </div>
   </div>
@@ -62,92 +61,104 @@ export default {
 
 <style lang="scss">
 .fullscreen{
-    z-index: 10;
-    background: -webkit-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
-    background: -moz-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
-    background: -ms-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
-    background: -o-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
-    background: radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
-    position: fixed;
-    top: 0;
-    left: 0;
+  z-index: 10;
+  background: -webkit-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
+  background: -moz-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
+  background: -ms-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
+  background: -o-radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
+  background: radial-gradient(circle at center,rgba(0,0,0,.8),rgba(0,0,0,.94));
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  .wrapper-header{
+    position: relative;
+    max-width: 1086px;
+    margin: auto;
+  }
+
+  header{
+    display: grid;
+    grid-template-columns: 40px 1fr 40px;
     width: 100%;
-    height: 100%;
+    grid-column-gap: 24px;
+    align-items: center;
+    padding: 12px 6px;
 
-  .wrapper-fullscreen{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    button.controls{
+      cursor: pointer;
+      padding: 20px;
+      background-image: url('~@/assets/img/arrow.svg');
+      background-repeat: no-repeat;
+      background-position: center;
+      width: 40px;
+      height: 60px;
+      background-color: rgba(255, 255, 255, 0.7);
+      border-radius: 3px 100% 100% 3px;
+      border: none;
+      outline: 0;
 
-    .controls{
+      &:hover{
+        background-color: rgba(255, 255, 255, 1);
+      }
 
-      button{
-        position: absolute;
-        cursor: pointer;
-        padding: 20px;
-        top: 50%;
-        background-image: url('~@/assets/img/arrow.svg');
-        background-repeat: no-repeat;
-        background-position: center;
-        width: 40px;
-        height: 60px;
-        background-color: rgba(255, 255, 255, 0.7);
-        border-radius: 3px 100% 100% 3px;
-        border: none;
-        outline: 0;
-
-        &:hover{
-          background-color: rgba(255, 255, 255, 1);
-        }
-
-        &.left{
-          left: 90px;
-          transform: rotate(180deg);
-        }
-
-        &.right{
-          right: 90px;
-        }
+      &.left{
+        transform: rotate(180deg);
       }
     }
 
-    .view{
+    .text{
       display: grid;
       grid-template-columns: 100%;
-      grid-template-rows: 100px auto;
-      justify-items: center;
+      grid-template-rows: 1fr;
+      grid-row-gap: 2px;
+      justify-content: start;
+      justify-items: start;
+      align-items: start;
 
-      .text{
-        justify-self: start;
-        width: 1086px;
-        margin: auto;
-        display: grid;
-        grid-template-columns: 100%;
-        grid-template-rows: 1fr;
-        grid-row-gap: 12px;
-        justify-content: start;
-        justify-items: start;
-        align-items: start;
-
-        h2, p{
-          background: #fff;
-        }
-
-        h2{
-          padding: 6px 12px;
-          font-size: 24px;
-        }
-        p {
-          padding: 3px 6px;
-          min-height: 16px;
-        }
+      h2, p{
+        background: #fff;
       }
-      .pic{
-        img{
-          max-height: 768px;
-        }
+
+      h2{
+        padding: 6px 12px;
+        font-size: 24px;
       }
+      p {
+        padding: 3px 6px;
+        min-height: 16px;
+      }
+    }
+  }
+
+  .wrapper-view{
+    max-height: calc(100% - 84px - 12px);
+    height: 100%;
+    position: relative;
+    max-width: 1086px;
+    margin: auto;
+  }
+
+  .view{
+    width: 100%;
+    height: 100%;
+
+    img{
+      max-height: 100%;
+      margin: auto;
+    }
+  }
+}
+@media only screen and (min-width: 1024px) and (min-height: 900px) {
+  .fullscreen{
+
+    .wrapper-header{
+      margin: 3 * 12px auto 0;
+    }
+    .wrapper-view{
+      max-height: calc(100% - 84px - 12px - 3 * 12px);
     }
   }
 }
